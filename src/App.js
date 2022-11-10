477347b9f77319c22f7a6375d1adcc30b157ce81
Erik <erik.vainumae@gmail.com>
2022-11-10 15:02:06 +0200

    made team view as a module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Teams from './components/Teams';
-import Activities from './components/Activities'
+import Activities from './components/Activities';
+import Team from './components/Team';
+
 
 function App() {
     return (
         <div className="App">
             <Teams />
             <Activities></Activities>
+            <Team/>
         </div>
     );
 }
