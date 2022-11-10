895d8e5686407f3b8b8fda69ce556b44f4b89586
marwanfatalla <117901909+marwanfatalla@users.noreply.github.com>
2022-11-10 11:45:25 +0100

    -Added Activity and Activities Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Model1 from './components/Model1';
+import Activities from './components/Activities'
 
 function App() {
     return (
         <div className="App">
             <Model1 />
+            <Activities></Activities>
         </div>
     );
 }
