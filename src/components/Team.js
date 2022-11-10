b35758604fe0c8d7dc9aa2c29cff9c58291c9cca
Erik Vainumae <erik.vainumae@solera.com>
2022-11-10 12:47:41 +0200

    first team prieview commit

---
+++ src/components/Team.js
@@ -0,0 +1,17 @@
+import './Team.css'
+
+const Team = () => {
+  return (
+
+    <div className="main-container">
+      <div className="team-container">
+        <div className="team-header"><h2>Team 5 </h2> <button className="btn">Edit</button></div>
+        <div className="score">100</div>
+      </div>
+      <div><button className="back"> <a href="#">Back</a></button></div>
+    </div>
+
+  );
+};
+
+export default Team;
