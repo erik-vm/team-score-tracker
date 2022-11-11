5d0498d46b93fbbea280f05d7933bc858c98e655
marwanfatalla <117901909+marwanfatalla@users.noreply.github.com>
2022-11-11 09:55:57 +0100

    -Fixed conflict

--- src/components/Team.js
+++ src/components/Team.js
@@ -2,11 +2,13 @@ import React, { Component, useState } from 'react';
 import './Team.css'
 
 const Team = ({name, points}) => {
+  
+
 
   const modelToggler = () => {
     var mainContainer = document.querySelector(".main-container");
     mainContainer.classList.toggle("show");
-
+  }
   const clickTeamHandler = () => {
     alert(`You clicked on ${name}`)
   }
@@ -19,15 +21,6 @@ const Team = ({name, points}) => {
     </div>
   );
 
-    {/* <div className="main-container">
-      <div className="team-container">
-        <div className="team-header"><h2>Team 5 </h2> <button className="btn">Edit</button></div>
-        <div className="score">100</div>
-      </div>
-      <div><button className="close" onClick={modelToggler}>X</button></div>
-    </div>
- */}
-  
 };
 
 export default Team;
