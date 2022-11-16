2f4a3731a07d90acebaf04ba1d213d31e80250c3
Luis Filipe <luisfelipecorino2@gmail.com>
2022-11-16 11:01:03 +0100

    It fetches the initial idnumber of the initial teams

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,14 @@
+import agent from '../data/agent';
+
 //returns an array of the 10 initial teams
 export function populateTeams() {
     const INITIAL_TEAM_SIZE = 10;
-    let initialTeams = []
+    let initialTeams = [];
     for(let i = 1; i < INITIAL_TEAM_SIZE + 1; i++ ){
-        initialTeams.push({name: `Team ${i}`, points:0})
+        //initialTeams.push({name: `Team ${i}`, points:0})
+        agent.activity.update(i);
+        initialTeams.push(agent.team.getById(i));
     }
+    //initialTeams.push(agent.team.getAll());
     return initialTeams;
 }
